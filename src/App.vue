2139644dca84097ff944ad7ad806ea4051a12464
ewi-orion <brpmdr@mailfence.com>
2022-08-05 23:44:21 -0400

    delete eslint | create Component Todo

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app">
+    <div class="container">
+      <h1>API</h1>
+      <h6>problemas</h6>
+
+      <Todo />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Todo from "./components/Todo.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
-  },
+    Todo,
+  }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-family: sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
 }
+
+.contaner {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
+}
+
 </style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <h3>Todos</h3>
+    <span class="legend">Doble click para marcar como completado.</span>
+    
+    <span>
+        <span class="incomplete-box"></span> = No completado
+    </span>
+
+    <span>
+        <span class="complete-box"></span> = Completado
+    </span>
+  </div>
+</template>
+30:55
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.todos {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 16px;
+}
+
+.todo {
+    border: 1px solid #ccc;
+    background: #41b883;
+    padding: 16px;
+    border-radius: 5px;
+    text-align: center;
+    position: relative;
+    cursor: pointer;
+}
+
+i {
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+    color: #fff;
+    cursor: pointer;
+}
+
+.legend {
+    display: flex;
+    justify-content: space-around;
+    margin-bottom: 16px;
+}
+
+.complete-box {
+    display:inline-block;
+    width: 10px;
+    height: 10px;
+    background: #35495e;
+}
+
+.incomplete-box {
+    display: inline-block;
+    width: 10px;
+    height: 10px;
+    background: #41b882;
+}
+
+@media (max-width: 500px) {
+    .todos {
+        grid-template-columns: 1fr;
+    }
+}
+</style>
